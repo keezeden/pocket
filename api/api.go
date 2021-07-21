fc3da3b98f578e0c9b19cfff1743d84968a07d70
Keez Eden <keezeden@gmail.com>
2021-07-22 11:03:25 +1200

    Pokeapi integration

---
+++ api/api.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+
+var endpoint = "https://pokeapi.co/api/v2/pokemon"
+
+func GetPokemonByEntry(entry int) (entryJson map[string]interface{}) {
+	request := fmt.Sprintf("%s/%d", endpoint, entry)
+	resp, err := http.Get(request)
+	
+	if err != nil {
+        log.Fatal(err)
+    }
+
+	//We Read the response body on the line below.
+	body, err := ioutil.ReadAll(resp.Body)
+	
+	if err != nil {
+	   log.Fatalln(err)
+	}
+
+	data := make(map[string]interface{})
+	json.Unmarshal(body, &data)
+	
+	if err != nil {
+		log.Fatalln(err)
+	 }
+
+	return data
+}
--- pocket.go
+++ pocket.go
@@ -4,13 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
 
+	"github.com/keezeden/pocket/api"
 	"github.com/keezeden/pocket/changelog"
 )
 
 func main() {
 	filepath := flag.String("file", "./CHANGELOG.md", "The file to transform into a pokedex entry.")
-
 	flag.Parse()
 	
 	version, err := changelog.GetVersion(filepath)
@@ -20,4 +22,16 @@ func main() {
 	}
 
 	fmt.Println(version)
+
+	segments := strings.Split(version, ".")
+
+	major, err := strconv.Atoi(segments[0])
+	minor, err := strconv.Atoi(segments[1])
+
+	entry := major + minor
+
+	pokemon := api.GetPokemonByEntry(entry)
+
+
+	fmt.Println(pokemon["name"])
 }
